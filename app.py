f4e997f7f238747254854166bd885415e2aa23ea
15077693d <15077693d@gmail.com>
2020-07-11 18:04:51 +0800

    add cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from api.student import StudentApi
+from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 db = MongoEngine()
 app = Flask(__name__)
+CORS(app)
 app.config["MONGODB_HOST"] = "mongodb+srv://test:test@cluster0.yjmzp.gcp.mongodb.net/student?retryWrites=true&w=majority"
 api = Api(app)
 api.add_resource(StudentApi, "/student")
